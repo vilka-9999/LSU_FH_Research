b0ab22fc1f9604d4e1bbdeb806c9e36f47620616
vilka-9999 <ilia.volkov.05@gmail.com>
2024-06-01 20:15:19 -0400

    Update data_analyzer.py

--- data_analyzer.py
+++ data_analyzer.py
@@ -5,19 +5,24 @@ import matplotlib.pyplot as plt
 
 def clean_data_year(df):
     years = []
+    # fills "years list" where index is a year's number and the element at that index is an index of the table row
+    # The index of a row is the last index of the row with the current year
     for i in range(len(df['Date']) - 1):
         if int(df['Date'][i+1]) - int(df['Date'][i]) > 50:
             years.append(i)
     years.append(len(df['Date']) - 1)
+    # fills the table rows by checking if an index smaller than index of the the last row with the same year
     def YearSetup(row):
         for y in range(len(years)):
             if row.name <= years[y]:
                 return int(y + 1)
+    # insert Year column        
     df.insert(0, 'Year', 0)
     df['Year'] = df.apply(YearSetup, axis=1)
     return df
 
 def clean_data_weekDay(df):
+    # fills each row based on the remainder of the division 'Date' by 7
     def WeekDaySetup(row):
         if int(row['Date']) % 7 == 4:
             return '3 Wen'
@@ -33,24 +38,27 @@ def clean_data_weekDay(df):
             return '1 Mon'
         elif int(row['Date']) % 7 == 3:
             return '2 Tue'
+    # insert WeekDay column
     df.insert(2, 'WeekDay', 0)
     df['WeekDay'] = df.apply(WeekDaySetup, axis=1)
     return df
 
 def clean_data_week(df):
     weeks = []
+    # fills "weeks list" where index is a week's number and the element at that index is an index of the table row
+    # The index of a row is the last index of the row with the current week
     for i in range(len(df['Date']) - 1):
         if int(df['WeekDay'][i+1][0]) - int(df['WeekDay'][i][0]) < 0:
             weeks.append(i)
         elif int(df['Year'][i+1]) != int(df['Year'][i]):
             weeks.append(i)
     weeks.append(i)
-
+    # fills the table rows by checking if index smaller than index of the the last row with the same week
     def WeekSetup(row):
         for w in range(len(weeks)):
             if row.name <= weeks[w]:
                 return int(w + 1)
-        
+    # insert Week column     
     df.insert(1, 'Week', 0)
     df['Week'] = df.apply(WeekSetup, axis=1)
     return df
@@ -64,9 +72,8 @@ def clean_data(df):
 
 
 def main():
-    # load data
-    data = pandas.read_csv('data\All Data as of 2024.05.30 csv.csv')
-    clean_df = clean_data(data)
+    data = pandas.read_csv('data\All Data as of 2024.05.30 csv.csv') # load data
+    clean_df = clean_data(data)  # clean data
     #clean_df.to_csv('data_cleaned/clean_data.csv', encoding='utf-8', index=False) # save cleaned data to scv
     
 
